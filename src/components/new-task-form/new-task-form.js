0607f68ad190bed46ce46ddcf59195a6d2a0f09c
gojimor <jgthjgth@gmail.com>
2022-12-14 15:37:42 +0300

    first commit

--- src/components/new-task-form/new-task-form.js
+++ src/components/new-task-form/new-task-form.js
@@ -24,6 +24,7 @@ export default class NewTaskForm extends Component {
     e.preventDefault();
     this.props.addTask(this.state.description);
     this.setState({ description: '' });
+    console.log('asd');
   };
 
   render() {
