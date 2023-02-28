c59f3004770528f11d0115d45535e66195253ab9
gojimor <jgthjgth@gmail.com>
2023-02-28 17:56:10 +0300

    task.js on hooks

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,62 +1,67 @@
 import EditTaskForm from '../edit-task-form';
 import Timer from '../timer/timer';
-import { Component } from 'react';
 import { formatDistanceToNow } from 'date-fns';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-export default class Task extends Component {
-  static defaultProps = {
-    id: '',
-    description: '',
-    timeStamp: '',
-    onCompleted: () => {},
-    onDeleted: () => {},
-    onEditingSubmit: () => {},
+export default function Task({
+  id,
+  description,
+  minutes,
+  seconds,
+  timeStamp,
+  onCompleted,
+  onDeleted,
+  onEditingSubmit,
+}) {
+  const [edit, setEdit] = useState(false);
+
+  const onEditedClick = () => {
+    setEdit(true);
   };
 
-  static propTypes = {
-    id: PropTypes.string,
-    description: PropTypes.string,
-    timeStamp: PropTypes.number,
-    onCompleted: PropTypes.func,
-    onDeleted: PropTypes.func,
-    onEditingSubmit: PropTypes.func,
+  const hiddenEditTaskForm = () => {
+    setEdit(false);
   };
 
-  state = { edit: false };
-
-  onEditedClick = () => {
-    this.setState({ edit: true });
-  };
-
-  hiddenEditTaskForm = () => {
-    this.setState({ edit: false });
-  };
-
-  render() {
-    const { id, description, minutes, seconds, timeStamp, onCompleted, onDeleted, onEditingSubmit } = this.props;
-    const { edit } = this.state;
-    if (edit) {
-      return (
-        <EditTaskForm
-          description={description}
-          onEditingSubmit={onEditingSubmit}
-          id={id}
-          hiddenEditTaskForm={this.hiddenEditTaskForm}
-        />
-      );
-    }
+  if (edit) {
     return (
-      <div className="view">
-        <input className="toggle" type="checkbox" onClick={onCompleted} />
-        <label>
-          <span className="description">{description}</span>
-          <Timer seconds={seconds} minutes={minutes} />
-          <span className="created">{formatDistanceToNow(timeStamp, { addSuffix: true })}</span>
-        </label>
-        <button type="button" className="icon icon-edit" onClick={this.onEditedClick} />
-        <button type="button" className="icon icon-destroy" onClick={onDeleted} />
-      </div>
+      <EditTaskForm
+        description={description}
+        onEditingSubmit={onEditingSubmit}
+        id={id}
+        hiddenEditTaskForm={hiddenEditTaskForm}
+      />
     );
   }
+  return (
+    <div className="view">
+      <input className="toggle" type="checkbox" onClick={onCompleted} />
+      <label>
+        <span className="description">{description}</span>
+        <Timer seconds={seconds} minutes={minutes} />
+        <span className="created">{formatDistanceToNow(timeStamp, { addSuffix: true })}</span>
+      </label>
+      <button type="button" className="icon icon-edit" onClick={onEditedClick} />
+      <button type="button" className="icon icon-destroy" onClick={onDeleted} />
+    </div>
+  );
 }
+
+Task.defaultProps = {
+  id: '',
+  description: '',
+  timeStamp: '',
+  onCompleted: () => {},
+  onDeleted: () => {},
+  onEditingSubmit: () => {},
+};
+
+Task.propTypes = {
+  id: PropTypes.string,
+  description: PropTypes.string,
+  timeStamp: PropTypes.number,
+  onCompleted: PropTypes.func,
+  onDeleted: PropTypes.func,
+  onEditingSubmit: PropTypes.func,
+};
